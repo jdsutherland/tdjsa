562f10d7ff4e4b99adcbe439bc36ab62a1553226
Jeff Sutherland <jeffdsuth@gmail.com>
2018-09-30 09:16:40 +0700

    Add positive palindrome tests

--- tdd/server/palindrome/test/palindrome-test.js
+++ tdd/server/palindrome/test/palindrome-test.js
@@ -7,11 +7,23 @@ describe('palindrome-test', function() {
   });
 
   it('should return true for argument mom', () => {
-    const aWord = 'mom';
+    expect(isPalindrome('mom')).to.be.true;
+  });
 
-    const result = isPalindrome(aWord);
+  it('should return true for argument dad', () => {
+    expect(isPalindrome('dad')).to.be.true;
+  });
 
-    expect(result).to.be.ok;
+  it('should return false for argument dude', () => {
+    expect(isPalindrome('dude')).to.be.false;
+  });
+
+  it('should return false for argument mom mom', () => {
+    expect(isPalindrome('mom mom')).to.be.true;
+  });
+
+  it('should return false for argument mom dad', () => {
+    expect(isPalindrome('mom dad')).to.be.false;
   });
 });
 
