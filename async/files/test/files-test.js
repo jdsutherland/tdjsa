97d42fcfd0a67508a35183286f3be2185946163b
Jeff Sutherland <jeffdsuth@gmail.com>
2018-09-30 10:41:30 +0700

    Fix async test using Mocha wait signal

--- async/files/test/files-test.js
+++ async/files/test/files-test.js
@@ -2,14 +2,12 @@ const expect = require('chai').expect;
 const linesCount = require('../src/files');
 
 describe('test server-side callback', function() {
-  it('should return correct lines count for a valid file', () => {
-    // Good try but this won't work
+  it('should return correct lines count for a valid file', (done) => {
     const callback = function(count) {
-      expect(count).to.eql(-2319);
+      expect(count).to.eql(16);
+      done();
     };
 
     linesCount('src/files.js', callback);
-    // doesn't actually wait for callback to be executed
-    // so test passes (lies!)
   });
 });
