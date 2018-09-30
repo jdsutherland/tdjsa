1afa77c00fbcb166815ebfdcbdfbc9c354637674
Jeff Sutherland <jeffdsuth@gmail.com>
2018-09-30 15:00:22 +0700

    Add spike

--- tackle/spike/src/locateme.js
+++ tackle/spike/src/locateme.js
@@ -1,2 +1,14 @@
 const locate = function() {
+  navigator.geolocation.getCurrentPosition(
+    function(position) {
+      const location = {
+        lat : position.coords.latitude,
+        lon : position.coords.longitude
+      };
+      const url = `http://maps.google.com/?q=${location.lat},${location.lon}`;
+      window.location = url;
+    },
+    function(){document.getElementById('error').innerHTML =
+        'unable to get your location'
+    });
 };
