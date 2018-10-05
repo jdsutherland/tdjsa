1c6dbdd3ec786871812abd6957489fc7722a9f07
Jeff Sutherland <jeffdsuth@gmail.com>
2018-10-05 11:45:08 +0700

    Add all & get tests

--- testexpress/todo/models/task.js
+++ testexpress/todo/models/task.js
@@ -1,2 +1,19 @@
+const db = require('../db');
+const ObjectId = require('mongodb').ObjectId;
+
+const collectionName = 'tasks';
+
 module.exports = {
+  all: function(callback) {
+    db.get().collection(collectionName).find().toArray(callback);
+  },
+
+  get: function(taskId, callback) {
+    try {
+      db.get().collection(collectionName)
+        .find({'_id': new ObjectId(taskId)}).limit(1).next(callback);
+    } catch (ex) {
+      callback(ex);
+    }
+  },
 };
--- testexpress/todo/test/server/models/task-test.js
+++ testexpress/todo/test/server/models/task-test.js
@@ -21,9 +21,9 @@ describe('task model tests', () => {
     sampleTask = {name: 'a new task', month: 12, day: 10, year: 2016};
 
     sampleTasks = [
-      {_id: id('123412341240'), name: 'a new task', month: 10, day: 1, year: 2016},
-      {_id: id('123412341241'), name: 'a new task', month: 11, day: 2, year: 2016},
-      {_id: id('123412341242'), name: 'a new task', month: 12, day: 3, year: 2016},
+      {_id: id('123412341240'), name: 'task1', month: 10, day: 1, year: 2016},
+      {_id: id('123412341241'), name: 'task2', month: 11, day: 2, year: 2016},
+      {_id: id('123412341242'), name: 'task3', month: 12, day: 3, year: 2016},
     ];
 
     db.get().collection('tasks').insert(sampleTasks, done);
@@ -36,4 +36,42 @@ describe('task model tests', () => {
   it('should pass this canary test', () => {
     expect(true).to.eql(true);
   });
+
+  it('all should return all the tasks', (done) => {
+    const callback = function(err, tasks) {
+      expect(tasks).to.eql(sampleTasks);
+      done();
+    };
+
+    task.all(callback);
+  });
+
+  it('get should return task with given id', (done) => {
+    const callback = function(err, task) {
+      expect(task.name).to.eql('task1');
+      expect(task.month).to.eql(10);
+      done();
+    };
+
+    task.get('123412341240', callback);
+  });
+
+  it('get should return null for non-existing task', (done) => {
+    const callback = function(err, task) {
+      expect(task).to.be.null
+      done();
+    };
+
+    task.get('666666666666', callback);
+  });
+
+  it('get should return error on invalid taskId input', (done) => {
+    const callback = function(err, task) {
+      expect(err).to.be.instanceof(Error);
+      done();
+    };
+
+    task.get('NOPE', callback);
+  });
+
 });
