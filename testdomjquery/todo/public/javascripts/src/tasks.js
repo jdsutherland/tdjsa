2d76e2c500fa7568af1fda3e2f4fee59216a1194
Jeff Sutherland <jeffdsuth@gmail.com>
2018-10-08 14:41:09 +0700

    Add updateTasks

--- testdomjquery/todo/public/javascripts/src/tasks.js
+++ testdomjquery/todo/public/javascripts/src/tasks.js
@@ -4,4 +4,21 @@ var getTasks = (callback) => {
 
 var callService = () => {};
 
-var updateTasks = () => {};
+var updateTasks = (status, response) => {
+	if (status !== 200) {
+		const message = `${response} (status: ${status})`;
+		document.getElementById('message').innerHTML = message;
+		return;
+	}
+
+	const tasks = JSON.parse(response);
+	document.getElementById('taskscount').innerHTML = tasks.length;
+
+	var row = (task) => {
+		return `<tr><td>${task.name}</td>
+			<td>${task.month}/${task.day}/${task.year}</td></tr>`;
+	}
+
+	var table = `<table>${tasks.map(row).join('')}</table>`;
+	document.getElementById('tasks').innerHTML = table;
+};
--- testdomjquery/todo/test/client/tasks-test.js
+++ testdomjquery/todo/test/client/tasks-test.js
@@ -1,8 +1,22 @@
 describe('tasks-with builtin functions-tests', () => {
   let sandbox;
+  let responseStub;
 
   beforeEach(function() {
     sandbox = sinon.sandbox.create();
+
+    domElements = {};
+
+    sandbox.stub(document, 'getElementById', id => {
+      if (!domElements[id]) domElements[id] = {};
+      return domElements[id];
+    });
+
+    responseStub = JSON.stringify([
+      {_id: '123412341201', name: 'task a', month: 8, day: 1, year: 2016},
+      {_id: '123412341202', name: 'task b', month: 9, day: 10, year: 2016},
+      {_id: '123412341203', name: 'task c', month: 10, day: 11, year: 2017},
+    ]);
   });
 
   afterEach(function() {
@@ -26,5 +40,26 @@ describe('tasks-with builtin functions-tests', () => {
 
     getTasks();
     callServiceMock.verify();
+  });
+
+  it('updateTasks should update message if status != 200', () => {
+    updateTasks(404, '..err..');
+
+    expect(domElements.message.innerHTML).to.eql('..err.. (status: 404)');
+  });
+
+  it('updateTasks should update taskscount', () => {
+    updateTasks(200, responseStub);
+
+    expect(domElements.taskscount.innerHTML).to.eql(3);
+  });
+
+  it('updateTasks should update the tasks table', () => {
+    updateTasks(200, responseStub);
+
+    expect(domElements.tasks.innerHTML).contains('<table>');
+    expect(domElements.tasks.innerHTML).contains('<td>task a</td>');
+    expect(domElements.tasks.innerHTML).contains('<td>8/1/2016</td>');
+    expect(domElements.tasks.innerHTML).contains('<td>task b</td>');
   });
 });
