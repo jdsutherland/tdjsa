65821b353ea37eaae544b4b7ac565d6b29c59695
Jeff Sutherland <jeffdsuth@gmail.com>
2018-10-12 15:07:15 +0700

    Implement sort

--- testangular2/todo/public/src/app/tasks/tasks-sort.pipe.js
+++ testangular2/todo/public/src/app/tasks/tasks-sort.pipe.js
@@ -5,7 +5,10 @@
       constructor: function() {},
       transform: function(tasks) {
         const compareTwoTasks = (task1, task2) => {
-          return task1.year - task2.year;
+          return task1.year - task2.year
+            || task1.month - task2.month
+            || task1.day - task2.day
+            || task1.name.localeCompare(task2.name);
         };
 
         return tasks.sort(compareTwoTasks);
--- testangular2/todo/test/client/app/tasks/tasks-sort.pipe-test.js
+++ testangular2/todo/test/client/app/tasks/tasks-sort.pipe-test.js
@@ -19,5 +19,37 @@ describe('tasks-sort pipe tests', function() {
 
     expect(sorted).to.eql([task2, task1]);
   });
+
+  it('should sort tasks based on year, then month', () => {
+    const task1 = {name: 'task a', month: 1, day: 10, year: 2017};
+    const task2 = {name: 'task b', month: 2, day: 10, year: 2016};
+    const task3 = {name: 'task c', month: 1, day: 10, year: 2016};
+
+    const sorted = sortPipe.transform([task1, task2, task3]);
+
+    expect(sorted).to.eql([task3, task2, task1]);
+  });
+
+  it('should sort tasks based on year, month then day', () => {
+    const task1 = {name: 'task a', month: 1, day: 13, year: 2016};
+    const task2 = {name: 'task b', month: 1, day: 10, year: 2016};
+    const task3 = {name: 'task c', month: 1, day: 12, year: 2016};
+
+    const sorted = sortPipe.transform([task1, task2, task3]);
+
+    expect(sorted).to.eql([task2, task3, task1]);
+  });
+
+  it('should sort tasks based on year, month, day then name', () => {
+    const task1 = {name: 'task b', month: 1, day: 10, year: 2016};
+    const task2 = {name: 'task c', month: 1, day: 10, year: 2016};
+    const task3 = {name: 'task d', month: 1, day: 3, year: 2015};
+    const task4 = {name: 'task a', month: 1, day: 10, year: 2016};
+
+    const sorted = sortPipe.transform([task1, task2, task3, task4]);
+
+    expect(sorted).to.eql([task3, task4, task1, task2]);
+  });
+
 });
 
