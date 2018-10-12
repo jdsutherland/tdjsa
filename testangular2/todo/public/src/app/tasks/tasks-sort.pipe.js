2f3105be28364337af1491715e08f8ec2cd646d0
Jeff Sutherland <jeffdsuth@gmail.com>
2018-10-12 14:40:27 +0700

    Add sort by year

--- testangular2/todo/public/src/app/tasks/tasks-sort.pipe.js
+++ testangular2/todo/public/src/app/tasks/tasks-sort.pipe.js
@@ -3,8 +3,12 @@
     .Pipe({ name: 'sort' })
     .Class({
       constructor: function() {},
-      // transform: function(tasks) {
-      //   return tasks.slice().sort();
-      // }
+      transform: function(tasks) {
+        const compareTwoTasks = (task1, task2) => {
+          return task1.year - task2.year;
+        };
+
+        return tasks.sort(compareTwoTasks);
+      }
     });
 }(window.app || (window.app = {})));
--- testangular2/todo/test/client/app/tasks/tasks-sort.pipe-test.js
+++ testangular2/todo/test/client/app/tasks/tasks-sort.pipe-test.js
@@ -11,5 +11,13 @@ describe('tasks-sort pipe tests', function() {
     expect(annotations.name).to.eql('sort');
   });
 
+  it('should sort tasks based on year', () => {
+    const task1 = {name: 'task a', month: 1, day: 10, year: 2017};
+    const task2 = {name: 'task b', month: 1, day: 10, year: 2016};
+
+    const sorted = sortPipe.transform([task1, task2]);
+
+    expect(sorted).to.eql([task2, task1]);
+  });
 });
 
