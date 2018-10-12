95858ac2192b67ab68b807ae86a47dc333940bd2
Jeff Sutherland <jeffdsuth@gmail.com>
2018-10-12 12:06:57 +0700

    Add getTasks register handler test
    High effort test but allows us to verify getTasks without real service.

--- testangular2/todo/public/src/app/tasks/tasks.component.js
+++ testangular2/todo/public/src/app/tasks/tasks.component.js
@@ -5,9 +5,20 @@
       templateUrl:'tasks.component.html',
     })
     .Class({
-      constructor: function() {
+      constructor: function(_tasksService) {
         this.tasks = [];
         this.messages = '';
-      }
+        this.service = _tasksService;
+      },
+      getTasks: function() {
+        this.service.get()
+          .subscribe(
+            this.updateTasks.bind(this),
+            this.updateError.bind(this)
+          );
+      },
+      updateTasks: function() {},
+      updateError: function() {},
+
     });
 })(window.app || (window.app = {}));
--- testangular2/todo/test/client/app/tasks/tasks.component-test.js
+++ testangular2/todo/test/client/app/tasks/tasks.component-test.js
@@ -1,9 +1,35 @@
 let tasksComponent;
+let sandbox;
+let tasksService;
+
+const observable = { subscribe: function() {} };
+const updateTasksBindStub = () => {};
+const updateErrorBindStub = () => {};
 
 beforeEach(function() {
-  tasksComponent = new app.TasksComponent();
+  tasksService = {
+    get: () => {},
+    add: () => {},
+    delete: () => {},
+  };
+  tasksComponent = new app.TasksComponent(tasksService);
+
+  sandbox = sinon.sandbox.create();
+
+  sandbox.stub(tasksComponent.updateTasks, 'bind')
+    .withArgs(tasksComponent)
+    .returns(updateTasksBindStub);
+
+  sandbox.stub(tasksComponent.updateError, 'bind')
+    .withArgs(tasksComponent)
+    .returns(updateErrorBindStub);
+
+  sandbox.stub(tasksService, 'get').withArgs().returns(observable);
 });
 
+afterEach(function() {
+  sandbox.restore();
+});
 
 describe('tasks component tests', function() {
   it('should set the selector attribute', () => {
@@ -26,6 +52,16 @@ describe('tasks component tests', function() {
 
   it('should initialize messages to empty string', () => {
     expect(tasksComponent.messages).to.eql('');
+  });
+
+  it('getTasks should register handlers with service', () => {
+    observableMock = sandbox.mock(observable)
+      .expects('subscribe')
+      .withArgs(updateTasksBindStub, updateErrorBindStub);
+
+    tasksComponent.getTasks();
+
+    observableMock.verify();
   });
 });
 
